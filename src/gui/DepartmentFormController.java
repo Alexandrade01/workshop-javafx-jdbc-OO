7b4aa9a92e8bbf0478bc34165e304225bc1bca48
Alexandrade01 <solidwinter96@outlook.com>
2022-10-21 16:48:33 -0300

    DepartmentForm controller

---
+++ src/gui/DepartmentFormController.java
@@ -0,0 +1,27 @@
+package gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+public class DepartmentFormController implements Initializable {
+
+	@FXML
+	private TextField txtId;
+	
+	@FXML
+	private TextField txtName;
+	
+	@FXML
+	private Label labelErrorName;
+	
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -1,80 +1,120 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 
 import application.Main;
+import gui.util.Alerts;
+import gui.util.Utils;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.entities.Department;
 import model.service.DepartmentService;
 
 public class DepartmentListController implements Initializable {
-	
+
 	private DepartmentService service;
-	
+
 	@FXML
 	private TableView<Department> tableViewDepartment;
-	
+
 	@FXML
 	private TableColumn<Department, Integer> tableColumnId;
-	
+
 	@FXML
-	private TableColumn<Department,String> tableColumnName;
-	
+	private TableColumn<Department, String> tableColumnName;
+
 	@FXML
 	private Button buttonNew;
-	
+
 	private ObservableList<Department> obsList;
-	
+
 	@FXML
-	public void onBtNewAction() {
-		
+	public void onBtNewAction(ActionEvent event) {
+		Stage parentStage = Utils.currentStage(event);
+		createDialogForm("/gui/DepartmentForm.fxml",parentStage);
 		System.out.println("onBtNewAction");
 	}
-	
+
 	public void setDepartmentService(DepartmentService service) {
-		
-		this.service=service; 
-		
+
+		this.service = service;
+
 	}
-	
+
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
-		
+
 		initializeNodes();
 	}
-	
-	//setando as colunas de acordo com o nome dos atributos das classes
+
+	// setando as colunas de acordo com o nome dos atributos das classes
 	private void initializeNodes() {
-		
+
 		tableColumnId.setCellValueFactory(new PropertyValueFactory<>("id"));
 		tableColumnName.setCellValueFactory(new PropertyValueFactory<>("name"));
-		
-		//serve para que a lista acompanhe ate o final da tela
+
+		// serve para que a lista acompanhe ate o final da tela
 		Stage stage = (Stage) Main.getMainScene().getWindow();
 		tableViewDepartment.prefHeightProperty().bind(stage.heightProperty());
 
 	}
-	
+
 	public void updateTableView() {
-		
-		if(service==null) {
-			
-			throw new IllegalStateException("Service was null");			
+
+		if (service == null) {
+
+			throw new IllegalStateException("Service was null");
 		}
 		List<Department> list = service.findAll();
 		obsList = FXCollections.observableArrayList(list);
 		tableViewDepartment.setItems(obsList);
 	}
-
 	
+	private void createDialogForm(String absoluteName,Stage parentStage) {
+		
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			
+			//carrega a view
+			Pane pane = loader.load();
+			//quando quero abrir uma janela nova preciso instanciar um novo stage
+			Stage dialogStage = new Stage();
+			
+			//setando o titulo do stage
+			dialogStage.setTitle("Enter Department data");
+			//
+			dialogStage.setScene(new Scene(pane));
+			//propriedade quem diz se a janela PODE OU NAO ser redimensionada
+			dialogStage.setResizable(false);
+			//indica quem é o stage pai dessa janela
+			dialogStage.initOwner(parentStage);
+			// window modal trava o app e so podemos usar o stage atual
+			dialogStage.initModality(Modality.WINDOW_MODAL);
+			//abre e espera a conclusao
+			dialogStage.showAndWait();
+		}
+		catch(IOException e) {
+			
+			Alerts.showAlert("Io Exception", "Error loading view",e.getMessage() , AlertType.ERROR);
+			
+		}
+		
+	}
+
 }
