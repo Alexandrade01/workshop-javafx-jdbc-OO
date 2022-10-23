87f125f79d8915898fa7d5fbe775e56bab1128ad
Alexandrade01 <solidwinter96@outlook.com>
2022-10-23 20:25:17 -0300

    SellerList

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -17,6 +17,7 @@ import javafx.scene.control.MenuItem;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.VBox;
 import model.service.DepartmentService;
+import model.service.SellerService;
 
 public class MainViewController implements Initializable {
 
@@ -30,19 +31,27 @@ public class MainViewController implements Initializable {
 	@FXML
 	public void onMenuItemSellerAction() {
 		System.out.println("onMenuItemSellerAction");
+		loadView("/gui/SellerList.fxml", (SellerListController controller) -> {
+
+			controller.setSellerService(new SellerService());
+			controller.updateTableView();
+		});
 	}
 
 	@FXML
 	public void onMenuItemDepartmentAction() {
-		loadView("/gui/DepartmentList.fxml", (DepartmentListController controller) -> { controller.setDepartmentService(new DepartmentService());
-		controller.updateTableView(); 
+		loadView("/gui/DepartmentList.fxml", (DepartmentListController controller) -> {
+
+			controller.setDepartmentService(new DepartmentService());
+			controller.updateTableView();
 		});
 	}
 
 	@FXML
 	public void onMenuItemAboutAction() {
 		System.out.println("onMenuItemAboutAction");
-		loadView("/gui/About.fxml", x -> {});
+		loadView("/gui/About.fxml", x -> {
+		});
 	}
 
 	@Override
@@ -53,9 +62,10 @@ public class MainViewController implements Initializable {
 
 	// synchronized garante que todos os processos da tela nao vao ser interrompidos
 	// na troca de telas
-	
-	//colocamos um segundo parametro no padrao lambda e generics para poder criar diferentes aberturas na mesma view loadView
-	private synchronized <T> void loadView(String absoluteName, Consumer<T> initializingAction ) {
+
+	// colocamos um segundo parametro no padrao lambda e generics para poder criar
+	// diferentes aberturas na mesma view loadView
+	private synchronized <T> void loadView(String absoluteName, Consumer<T> initializingAction) {
 
 		try {
 
@@ -73,18 +83,16 @@ public class MainViewController implements Initializable {
 			// remonta os childrens com os da tela principal e em seguida da tela about.fxml
 			mainVBox.getChildren().add(mainMenu);
 			mainVBox.getChildren().addAll(newVBox.getChildren());
-			
+
 			T controller = loader.getController();
 			initializingAction.accept(controller);
-			
-			
+
 		} catch (IOException e) {
 
 			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
 		}
 	}
-	
-	
+
 //	private synchronized void loadView2(String absoluteName) {
 //
 //		try {
